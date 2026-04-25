7b802ec6754463376631366c9f4190cf891b9410
Martin Obrátil <martin.obratil@gmail.com>
2026-04-25 10:35:36 +0200

    Update pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,21 +1,7 @@
-// Při startu zobraz smajlíka
-basic.showIcon(IconNames.Happy)
-
-// Tlačítko A — zobraz text
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.TShirt)
+})
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Ahoj!")
+	
 })
-
-// Tlačítko B — zobraz srdíčko
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(1000)
-    basic.showIcon(IconNames.Happy)
-})
-
-// Zatřesení — zobraz náhodné číslo 1-6 (kostka)
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
-    basic.pause(2000)
-    basic.showIcon(IconNames.Happy)
-})
+radio.setGroup(1)
